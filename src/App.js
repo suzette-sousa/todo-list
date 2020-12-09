a699630e235ec65109e01a32092ad15e8e29ae29
Suzette Sousa <sousa.suzette.pro@gmail.com>
2020-12-09 10:34:25 +0100

    create todos component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        title: 'Task 1',
+        finished: false
+      },
+      {
+        id: 2,
+        title: 'Task 2',
+        finished: false
+      },
+      {
+        id: 3,
+        title: 'Task 3',
+        finished: false
+      },
+    ]
+  }
+  render () {
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
